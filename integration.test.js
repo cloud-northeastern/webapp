b23e643f2451b343a882911a78ddffee138d6092
aakashrajawat00 <rajawat.a@northeastern.edu>
2023-10-04 19:01:43 -0400

    Added integration test

---
+++ integration.test.js
@@ -0,0 +1,18 @@
+const request = require('supertest');
+const app = require('index'); // Adjust the path based on your project structure
+
+process.env.DB_DIALECT = 'postgres';
+process.env.DB_HOST = 'localhost';
+process.env.DB_USERNAME = 'aakashrajawat';
+process.env.DB_PASSWORD = 'aakashrajawat';
+process.env.DB_NAME = 'cloud';
+process.env.DB_PORT = '5000';
+
+
+describe('Integration Tests', () => {
+  it('should return a success response from /healthz', async () => {
+    const response = await request(app).get('/healthz');
+    expect(response.status).toBe(200);
+    expect(response.body.status).toBe('ok');
+  });
+});
