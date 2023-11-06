15695657f19945917e9d9b96a61d3e36fdff7d03
aakashrajawat00 <rajawat.a@northeastern.edu>
2023-11-06 01:16:43 -0500

    dialect issues resolved

--- config/database.js
+++ config/database.js
@@ -1,33 +1,15 @@
-// // config/database.js
-
-// const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize({
-//   dialect: 'postgres', 
-//   host: 'localhost',
-//   username: 'aakashrajawat',
-//   password: 'aakashrajawat',
-//   database: 'csye6225',
-//   port: 5432,
-// });
-
-// module.exports = sequelize;
-
 const Sequelize = require('sequelize');
 module.exports= new Sequelize(process.env.DB_POSTGRES,process.env.DB_USER,process.env.DB_PASSWORD,{
 logging: false,
 host: process.env.DB_HOST,
 port: process.env.DB_PORT,
 dialect: process.env.DB_DIALECT,
-operatorsAliases: 0,
 
-pool:{
+dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
 
-max:5,
-min:0,
-acquire: 30000,
-idle: 10000
-
-
-},
 });
